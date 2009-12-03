46441dc6225096cf7e0a56f91aa94358b09e8f08
Ivan Willig <ivan@mao.(none)>
2009-12-03 09:07:59 -0500

    a basic working example

--- jquery.map.js
+++ jquery.map.js
@@ -1,6 +1,6 @@
 /* 
 GeoQuery
-
+Ivan Willig
 */
 (function($){
    $.fn.map = function(options) {
@@ -8,7 +8,13 @@ GeoQuery
      
    }; 
    var options = $.extend(defaults,options);
-   alert(options);
+   var div = this[0].id ; 
+
+   var map = new OpenLayers.Map(div);
+   var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS",
+                    "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'} );
+   map.addLayer(layer);
+   map.setCenter(new OpenLayers.LonLat(5, 40),5);
    };
 })(jQuery);
 
