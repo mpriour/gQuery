3d062bd24c804138373237496d636119e47f1100
Ivan Willig <ivan@mao.(none)>
2009-12-03 09:46:54 -0500

    allows the user to set the center a map

--- jquery.map.js
+++ jquery.map.js
@@ -5,16 +5,20 @@ Ivan Willig
 (function($){
    $.fn.map = function(options) {
    var defaults = { 
-     
+       'center':  [0,0,1],  
    }; 
    var options = $.extend(defaults,options);
-   var div = this[0].id ; 
+   var MapOptions = { 
 
+   }
+  
+   // kind of hacky.. but works
+   var div = this[0].id ;   
    var map = new OpenLayers.Map(div);
    var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS",
                     "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'} );
    map.addLayer(layer);
-   map.setCenter(new OpenLayers.LonLat(5, 40),5);
+   map.setCenter(new OpenLayers.LonLat(options.center[0],options.center[1]),options.center[2]);
    };
 })(jQuery);
 
