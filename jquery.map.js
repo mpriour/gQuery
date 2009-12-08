35aea8f5efc85cb0d8238b9f96efc626c4a487ed
chrispatterson <dimmerswitch@gmail.com>
2009-12-08 14:31:05 -0600

    Popup code should have defaults within gQuery

--- jquery.map.js
+++ jquery.map.js
@@ -18,7 +18,9 @@ See documentation for more details
        'projection': 900913,
        'displayProjection': 4326, 
        'baseLayer': 'openstreetmap', 
-       'resolutions': [156543.03390625, 78271.516953125, 39135.7584765625, 19567.87923828125, 9783.939619140625, 4891.9698095703125, 2445.9849047851562, 1222.9924523925781, 611.4962261962891, 305.74811309814453, 152.87405654907226, 76.43702827453613, 38.218514137268066, 19.109257068634033, 9.554628534317017, 4.777314267158508, 2.388657133579254, 1.194328566789627, 0.5971642833948135, 0.29858214169740677, 0.14929107084870338, 0.07464553542435169, 0.037322767712175846, 0.018661383856087923, 0.009330691928043961, 0.004665345964021981, 0.0023326729820109904, 0.0011663364910054952, 5.831682455027476E-4, 2.915841227513738E-4, 1.457920613756869E-4]
+       'resolutions': [156543.03390625, 78271.516953125, 39135.7584765625, 19567.87923828125, 9783.939619140625, 4891.9698095703125, 2445.9849047851562, 1222.9924523925781, 611.4962261962891, 305.74811309814453, 152.87405654907226, 76.43702827453613, 38.218514137268066, 19.109257068634033, 9.554628534317017, 4.777314267158508, 2.388657133579254, 1.194328566789627, 0.5971642833948135, 0.29858214169740677, 0.14929107084870338, 0.07464553542435169, 0.037322767712175846, 0.018661383856087923, 0.009330691928043961, 0.004665345964021981, 0.0023326729820109904, 0.0011663364910054952, 5.831682455027476E-4, 2.915841227513738E-4, 1.457920613756869E-4],
+			 'onClick': 	function(event) {showPopup(event);},
+			'onUnclick': function(event) {closePopup(event);}
  
    }; 
    var options = $.extend(defaults,options);
@@ -95,10 +97,6 @@ See documentation for more details
             var onFeatureSelect = options.onClick;    
         
         } 
-        else {
-            log("onclick is null");  
-            var onFeatureSelect; 
-        } 
         if (options.onUnclick != null) { 
             var onFeatureUnselect = options.onUnclick; 
         }; 
@@ -129,14 +127,21 @@ See documentation for more details
        map.zoomToExtent(Extent);  
    } 
    
+   function showPopup(event) {
+       var mapObject = event.object.map ;
+       var pixel = mapObject.getPixelFromLonLat(event.feature.geometry.getBounds().getCenterLonLat()) ;
+       $('#feature').html($.fn.map.popupFormat("<p>" + event.feature.attributes.description + "</p>")).css({'top': pixel.x, 'left': pixel.y});
+       log(pixel); 
+   }; 
+
+   function closePopup(event) {
+       log(event); 
+   };
  };
-	$.fn.map.popup = function(options) {
-	    debug(this);
-	};
-	
-	$.fn.map.popup.format = function(txt) { // Allow overrides from outside the function
+
+	$.fn.map.popupFormat = function(markup) { // Allow overrides from outside the function
 	// do we want to also allow classnames to be passed in via options?
-	 return '<div class="gquery-popup">' + txt + '</div>';
+	 return '<div class="gquery-wrap"><div class="gquery-popup">' + markup + '</div></div>';
 	};
 })(jQuery);
 
